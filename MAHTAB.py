7ad441241e69729dd5866b1953f771e67f4a0d9f
SK BAAP G <111557947+skbaap@users.noreply.github.com>
2023-03-15 20:03:48 +0500

    Update MAHTAB.py

--- MAHTAB.py
+++ MAHTAB.py
@@ -1,1 +1,26 @@
-import SKALI
+
+import os, platform
+
+try:
+
+    import requests
+
+except:
+
+    os.system('pip install requests')
+
+import requests
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+    from SKALI import Main
+
+    Main()
+
+elif bit == '32bit':
+
+    print("[×] YOUR DEVICE NOT SUPPORTED THIS TOOL")
+
+    exit()
